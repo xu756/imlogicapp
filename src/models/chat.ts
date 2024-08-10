58a92f029cefd909f04a9685bb822e2fa42b7ad6
xu756 <756334744@qq.com>
2024-08-10 21:54:33 +0800

    保存聊天记录信息

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,1 +1,36 @@
+import { useSet, useSetState } from 'ahooks';
+
 // 全局共享数据示例
+export default () => {
+  const initMsg: Msg.Message[] = [];
+  const [lastMsg, setLastMsg] = useSetState<Msg.Message>({} as Msg.Message);
+  const [
+    chatMsgs,
+    { add: addChatMsg, remove: removeChatMsg, reset: resetChatMsg },
+  ] = useSet<Msg.Message>(initMsg);
+
+  const newChatMsg = (msg: Msg.Message) => {
+    addChatMsg(msg);
+    setLastMsg({
+      chat_id: 11,
+      chat_type: msg.chat_type,
+      group_id: msg.group_id,
+      link_id: msg.link_id,
+      msg_id: msg.msg_id,
+      msg_meta: msg.msg_meta,
+      msg_type: msg.msg_type,
+      sender: msg.sender,
+      receiver: msg.receiver,
+      timestamp: msg.timestamp,
+      content: msg.content,
+      media: msg.media,
+    });
+  };
+  return {
+    chatMsgs,
+    lastMsg,
+    newChatMsg,
+    removeChatMsg,
+    resetChatMsg,
+  };
+};
--- src/typings/msg/message.d.ts
+++ src/typings/msg/message.d.ts
@@ -7,12 +7,14 @@ declare namespace Msg {
     chat_type: ChatType;
     group_id: number;
     link_id: string;
-    msg_content: MsgContent;
     msg_id: string;
     msg_meta?: MsgMeta;
     msg_type: MsgType;
     sender: number;
+    receiver: number;
     timestamp: number;
+    content: string;
+    media: MediaType[];
   };
   /**
    * 消息类型
@@ -46,48 +48,57 @@ declare namespace Msg {
    * 消息内容
    *
    */
-  export type MsgContent = {
-    audio?: AudioType;
-    content?: string;
-    file?: FileType;
-    img?: ImageType[];
-    video?: VideoType;
-  };
+  // export type MsgContent = {
+  //   audio?: AudioType;
+  //   content?: string;
+  //   file?: FileType;
+  //   img?: ImageType[];
+  //   video?: VideoType;
+  // };
+
   /**
-   * 音频
+   * 媒体内容
    */
-  export type AudioType = {
-    duration: number;
+  export type MediaType = {
     uid: string;
     url: string;
   };
-  /**
-   * 文件
-   */
-  export type FileType = {
-    uid: string;
-    url: string;
-  };
-  /**
-   * 视频
-   */
-  export type VideoType = {
-    duration: number;
-    uid: string;
-    url: string;
-  };
-  /**
-   * 消息元数据
-   */
-  export type MsgMeta = {
-    detail: 'connect' | 'disconnect' | 'heartbeat';
-    version: string;
-  };
-  /**
-   * 图片
-   */
-  export type ImageType = {
-    uid: string;
-    url: string;
-  };
+
+  //   /**
+  //    * 音频
+  //    */
+  //   export type AudioType = {
+  //     duration: number;
+  //     uid: string;
+  //     url: string;
+  //   };
+  //   /**
+  //    * 文件
+  //    */
+  //   export type FileType = {
+  //     uid: string;
+  //     url: string;
+  //   };
+  //   /**
+  //    * 视频
+  //    */
+  //   export type VideoType = {
+  //     duration: number;
+  //     uid: string;
+  //     url: string;
+  //   };
+  //   /**
+  //    * 消息元数据
+  //    */
+  //   export type MsgMeta = {
+  //     detail: 'connect' | 'disconnect' | 'heartbeat';
+  //     version: string;
+  //   };
+  //   /**
+  //    * 图片
+  //    */
+  //   export type ImageType = {
+  //     uid: string;
+  //     url: string;
+  //   };
 }
