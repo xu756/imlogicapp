1e22498a6af1434bb2e7c58f85367b11e592db2e
xu756 <756334744@qq.com>
2024-08-13 10:18:08 +0800

    调整ui

--- src/components/ChatBox/ChatBody.tsx
+++ src/components/ChatBox/ChatBody.tsx
@@ -5,12 +5,14 @@ import MessageItem from '../Message';
 import ChatInput from './ChatInput';
 
 const ChatBody = () => {
-  let chat_id = 2222;
+  const chat_id = 2222;
+  const receiver = 21474836484;
   const { lastMsg } = useModel('chat', (model) => ({
     lastMsg: model.lastMsg,
   }));
   const [messges, { set }] = useMap<string, Msg.Message>([]);
   const chatBodyRef = useRef<HTMLDivElement>(null);
+  const { initialState } = useModel('@@initialState');
 
   useUpdateEffect(() => {
     if (lastMsg.chat_id === chat_id) {
@@ -23,16 +25,23 @@ const ChatBody = () => {
       chatBodyRef.current.scrollTop = chatBodyRef.current.scrollHeight;
     }
   }, [messges]);
+  const sendMsg = (msg: Msg.Message) => {
+    // send message to server
+  };
   return (
     <div className="chat-body">
-      <div className="chat-messages" ref={chatBodyRef}>
+      <div className="chat-message" ref={chatBodyRef}>
         {Array.from(messges).map(([msg_id, msg]) => (
           <MessageItem key={msg.msg_id} {...msg} />
         ))}
       </div>
       <div className="chat-line" />
       <div className="chat-input">
-        <ChatInput />
+        <ChatInput
+          chat_id={chat_id}
+          sender={initialState?.user.id || 0}
+          receiver={2}
+        />
       </div>
     </div>
   );
--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,15 +1,21 @@
 import { MsgType } from '@/typings/msg/enum';
-import './index.less';
+import { useModel } from '@umijs/max';
 import { MessageUserAvatar } from './user';
 const MessageItem = (msg: Msg.Message) => {
+  const { initialState } = useModel('@@initialState');
+
   switch (msg.msg_type) {
     case MsgType.Text:
       return (
-        <div key={msg.msg_id} className="chat-message" id={`msg-${msg.msg_id}`}>
-          <div className="chat-message-sender">
+        <div
+          key={msg.msg_id}
+          className={`chat-message-item chat-message-item-${initialState?.user?.id === msg.sender ? 'right' : 'left'}`}
+          id={`msg-${msg.msg_id}`}
+        >
+          <div className="chat-message-item-sender">
             <MessageUserAvatar userId={msg.sender} />
           </div>
-          <div className="chat-message-content">{msg.content}</div>
+          <div className="chat-message-item-content">{msg.content}</div>
         </div>
       );
     default:
