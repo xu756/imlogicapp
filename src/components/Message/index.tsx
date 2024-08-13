e06694003befe183987c81fded841855f935edac
xu756 <756334744@qq.com>
2024-08-13 15:13:43 +0800

    ✨ feat(message): 前端对消息分类

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,26 +1,50 @@
 import { MsgType } from '@/typings/msg/enum';
 import { useModel } from '@umijs/max';
-import { MessageUserAvatar } from './user';
-const MessageItem = (msg: Msg.Message) => {
-  const { initialState } = useModel('@@initialState');
+import { Image } from 'antd';
 
+import { MessageUserAvatar } from './user';
+const MessageItemContent = (msg: Msg.Message) => {
   switch (msg.msg_type) {
     case MsgType.Text:
       return (
-        <div
-          key={msg.msg_id}
-          className={`chat-message-item chat-message-item-${initialState?.user?.id === msg.sender ? 'right' : 'left'}`}
-          id={`msg-${msg.msg_id}`}
-        >
-          <div className="chat-message-item-sender">
-            <MessageUserAvatar userId={msg.sender} />
+        <div className="chat-message-item-content-text">{msg.content}</div>
+      );
+
+    case MsgType.Image:
+      return (
+        <div className="chat-message-item-content-image">
+          <div className="chat-message-item-content-image-text">
+            {msg.content}
           </div>
-          <div className="chat-message-item-content">{msg.content}</div>
+          <Image.PreviewGroup>
+            {msg.media.map((img, index) => {
+              return <Image key={index} width={200} src={img.url} />;
+            })}
+          </Image.PreviewGroup>
         </div>
       );
     default:
       return null;
   }
+};
+
+const MessageItem = (msg: Msg.Message) => {
+  const { initialState } = useModel('@@initialState');
+
+  return (
+    <div
+      key={msg.msg_id}
+      className={`chat-message-item chat-message-item-${initialState?.user?.id === msg.sender ? 'right' : 'left'}`}
+      id={`msg-${msg.msg_id}`}
+    >
+      <div className="chat-message-item-sender">
+        <MessageUserAvatar userId={msg.sender} />
+      </div>
+      <div className="chat-message-item-content">
+        <MessageItemContent {...msg} />
+      </div>
+    </div>
+  );
 };
 
 export default MessageItem;
